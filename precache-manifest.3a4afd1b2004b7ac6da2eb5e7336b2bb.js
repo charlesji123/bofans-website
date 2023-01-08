5f67fb1a1ce466557cc415ddee1665f0103b68a2
charlesji123 <bofanj@princeton.edu>
2023-01-08 10:30:15 -0500

    Deploy React app to GitHub Pages

---
+++ precache-manifest.3a4afd1b2004b7ac6da2eb5e7336b2bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96c758b5e0a875cc031",
+    "url": "/bofans-website/static/css/main.2af10053.chunk.css"
+  },
+  {
+    "revision": "e96c758b5e0a875cc031",
+    "url": "/bofans-website/static/js/main.e96c758b.chunk.js"
+  },
+  {
+    "revision": "8f29e9adcb9cb0ccf843",
+    "url": "/bofans-website/static/js/1.8f29e9ad.chunk.js"
+  },
+  {
+    "revision": "c74d7ba9b2c2cf9f349b",
+    "url": "/bofans-website/static/js/2.c74d7ba9.chunk.js"
+  },
+  {
+    "revision": "e5a3ddee72bc9a98deea",
+    "url": "/bofans-website/static/js/runtime~main.e5a3ddee.js"
+  },
+  {
+    "revision": "b75810485202c005eef59a78bcab9037",
+    "url": "/bofans-website/static/media/anime_pfp.b7581048.png"
+  },
+  {
+    "revision": "6af523691c6cbf4a1af3696938c08c3f",
+    "url": "/bofans-website/static/media/pfp.6af52369.jpg"
+  },
+  {
+    "revision": "c70aa88af9bc2e7522712388a7808fca",
+    "url": "/bofans-website/index.html"
+  }
+];
